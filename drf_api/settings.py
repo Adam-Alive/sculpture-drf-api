9f4b417ad7d131a8be2cc5d9ce2b50da91ea178b
Adam Alive <adamalive.email@gmail.com>
2024-08-29 15:37:33 +0000

    Add cloudinary API to env.py and update settings.py

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -11,6 +11,15 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+if os.path.exists('env.py'):
+    import env
+
+CLOUDINARY_STORAGE = {
+    'CLOUDINARY_URL': os.environ.get('CLOUDINARY_URL')
+}
+MEDIA_URL = '/media/'
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +34,8 @@ SECRET_KEY = 'django-insecure-gjei)onkpnfm7nk!uhl=$dmu2!!ql)w$f)4*1^%wt6o^=3ydka
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['8000-adamalive-sculpturedrfa-aw4zyb1un53.ws.codeinstitute-ide.net/',
+                ]
 
 
 # Application definition
@@ -36,7 +46,9 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'cloudinary_storage',
     'django.contrib.staticfiles',
+    'cloudinary',
 ]
 
 MIDDLEWARE = [
