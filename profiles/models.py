c3da43b437635a8f80566f80d360303d897326c1
Adam Alive <adamalive.email@gmail.com>
2024-09-04 14:33:42 +0000

    Import django signals and add create_profile function

--- profiles/models.py
+++ profiles/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 
 
@@ -9,7 +10,7 @@ class Profile(models.Model):
     name = models.CharField(max_length=255, blank=True)
     content = models.TextField(blank=True)
     image = models.ImageField(
-        upload_to='images/', default='../default_profile_urqbqj'
+        upload_to='images/', default='../default_profile_svvdmi'
     )
 
     class Meta:
@@ -17,3 +18,10 @@ class Profile(models.Model):
 
     def __str__(self):
         return f"{self.owner}'s profile"
+
+
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(owner=instance)
+
+post_save.connect(create_profile, sender=User)
