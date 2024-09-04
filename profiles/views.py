b5831138b413689114f0b47a7b5d0487df96d92f
Adam Alive <adamalive.email@gmail.com>
2024-09-04 17:06:54 +0000

    Add profile list view and add path to urls.py

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Profile
 
-# Create your views here.
+# Use class from CI DRF-API walkthrough.
+class ProfileList(APIView):
+    """
+    List all profiles.
+    There is no Create view (post method), as profile creation handled by django signals.
+    """
+    def get(self, request):
+        profiles = Profile.objects.all()
+        return Response(profiles)
