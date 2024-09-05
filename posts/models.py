80fe930ea4b0f5c360e066de34423f08c5b3db36
Adam Alive <adamalive.email@gmail.com>
2024-09-05 20:32:03 +0000

    Register posts in admin.py and migrate

--- posts/models.py
+++ posts/models.py
@@ -7,8 +7,7 @@ class Post(models.Model):
     """
     Post model, related to 'owner', i.e. a User instance.
     Default image set so that we can always reference image.url.
-    """
-    
+    """    
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
