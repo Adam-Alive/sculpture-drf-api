8584b7ab14bd6fa7e5517efce46c99ba219a929f
Adam Alive <adamalive.email@gmail.com>
2024-09-07 10:55:18 +0000

    Add LikeSerializer

---
+++ likes/serializers.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+from likes.models import Like
+
+
+# Use class from CI DRF-API walkthrough.
+class LikeSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the Like model.
+    The create method handles the unique constraint on 'owner' and 'post'.
+    """
+    owner = serializers.ReadOnlyField(source='owner.username')
+
+    class Meta:
+        model = Like
+        fields = ['id', 'created_at', 'owner', 'post']
