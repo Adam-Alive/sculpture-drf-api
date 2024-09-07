a726fde9f8ddbae3e8a9c567ed9a39da9eb1b79b
Adam Alive <adamalive.email@gmail.com>
2024-09-07 11:15:55 +0000

    Add LikeList and LikeDetail views

--- likes/views.py
+++ likes/views.py
@@ -1,1 +1,27 @@
+from rest_framework import generics, permissions
+from drf_api.permissions import IsOwnerOrReadOnly
+from .models import Like
+from .serializers import LikeSerializer
 
+
+# Use class from CI DRF-API walkthrough.
+class LikeList(generics.ListCreateAPIView):
+    """
+    List likes or create a like if logged in.
+    """
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    serializer_class = LikeSerializer
+    queryset = Like.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+# Use class from CI DRF-API walkthrough.
+class LikeDetail(generics.RetrieveDestroyAPIView):
+    """
+    Retrieve a like or delete it by id if you own it.
+    """
+    permission_classes = [IsOwnerOrReadOnly]
+    serializer_class = LikeSerializer
+    queryset = Like.objects.all()
