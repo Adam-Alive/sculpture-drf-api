019416c9d357cd88070f1a0f1441a20f0b45f00b
Adam Alive <adamalive.email@gmail.com>
2024-09-07 14:00:56 +0000

    Add FollowerList view

--- followers/views.py
+++ followers/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from drf_api.permissions import IsOwnerOrReadOnly
+from .models import Follower
+from .serializers import FollowerSerializer
 
-# Create your views here.
+
+# Use class from CI DRF-API walkthrough.
+class FollowerList(generics.ListCreateAPIView):
+    """
+    List all followers, ie. all instances of a user
+    following another user'.
+    Create a follower, ie. follow a user if logged in.
+    Perform_create: associate the current logged in user with a follower.
+    """
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    serializer_class = FollowerSerializer
+    queryset = Follower.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
