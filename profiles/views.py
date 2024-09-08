f08fa87c37ce93bdb79eb6b8d92730d93d562c25
Adam Alive <adamalive.email@gmail.com>
2024-09-08 08:19:26 +0000

    Add count fields for posts, followers, following

--- profiles/views.py
+++ profiles/views.py
@@ -1,4 +1,5 @@
-from rest_framework import generics
+from django.db.models import Count
+from rest_framework import generics, filters
 from drf_api.permissions import IsOwnerOrReadOnly
 from .models import Profile
 from .serializers import ProfileSerializer
@@ -10,7 +11,13 @@ class ProfileList(generics.ListAPIView):
     List all profiles.
     There is no create view as profile creation is handled by django signals.
     """
-    queryset = Profile.objects.all()
+    # Up until the annotate method, we have used queryset with all objects.
+    # queryset = Profile.objects.all()
+    queryset = Profile.objects.annotate(
+        posts_count=Count('owner__post', distinct=True),
+        followers_count=Count('owner__followed', distinct=True),
+        following_count=Count('owner__following', distinct=True)
+    ).order_by('-created_at')  
     serializer_class = ProfileSerializer
 
 
