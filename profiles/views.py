fd48bcca2e15a6376d5cefd9eee4837017862255
Adam Alive <adamalive.email@gmail.com>
2024-09-08 08:29:26 +0000

    Set OrderingFilter and add ordering_fields

--- profiles/views.py
+++ profiles/views.py
@@ -19,6 +19,16 @@ class ProfileList(generics.ListAPIView):
         following_count=Count('owner__following', distinct=True)
     ).order_by('-created_at')  
     serializer_class = ProfileSerializer
+    filter_backends = [
+        filters.OrderingFilter
+    ]
+    ordering_fields = [
+        'posts_count',
+        'followers_count',
+        'following_count',
+        'owner__following__created_at',
+        'owner__followed__created_at',
+    ]
 
 
 # Use class from CI DRF-API walkthrough.
