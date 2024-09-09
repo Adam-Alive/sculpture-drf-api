d28a6ef8979459d60830a8857dea10566a3facf4
Adam Alive <adamalive.email@gmail.com>
2024-09-09 16:02:09 +0000

    Add TalkDetail view and update urls.py

--- talks/urls.py
+++ talks/urls.py
@@ -3,4 +3,5 @@ from talks import views
 
 urlpatterns = [
     path('talks/', views.TalkList.as_view()),
+    path('talks/<int:pk>/', views.TalkDetail.as_view()),
    ]
--- talks/views.py
+++ talks/views.py
@@ -6,11 +6,21 @@ from .serializers import TalkSerializer
 
 class TalkList(generics.ListCreateAPIView):
     """
-    List all talk events and create event if an administrator.    
-    """       
+    List all talk events and create event if an administrator.
+    """  
     serializer_class = TalkSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     queryset = Talk.objects.all()
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
+
+
+class TalkDetail(generics.RetrieveUpdateDestroyAPIView):
+    """
+    User can retrieve a talk event.
+    Administrator can edit or delete a talk event.
+    """
+    permission_classes = [IsOwnerOrReadOnly]
+    queryset = Talk.objects.all()
+    serializer_class = TalkSerializer
