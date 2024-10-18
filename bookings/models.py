8bd3a68c95a27f5537909aa2506fd9be5a1be901
Adam Alive <adamalive.email@gmail.com>
2024-10-18 18:06:01 +0000

    Amend Booking model for testing

--- bookings/models.py
+++ bookings/models.py
@@ -1,14 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
-from talks.models import Talk
 
 
 class Booking(models.Model):
     """
     Booking model for user to register for talk events.
     """
-    owner = models.ForeignKey(User, on_delete=models.CASCADE)
-    talk = models.ForeignKey(Talk, on_delete=models.CASCADE)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)   
+    talk = models.CharField(max_length=200)
     name = models.CharField(max_length=100)
     email = models.EmailField(max_length=150)
     questions = models.TextField(blank=True)
