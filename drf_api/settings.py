b0a31e57d4230dede7df12ed5baba5de24ccf219
Adam Alive <adamalive.email@gmail.com>
2024-10-20 12:15:14 +0000

    Add DEBUG=True

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -75,7 +75,8 @@ REST_AUTH_SERIALIZERS = {
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEV' in os.environ
+# DEBUG = 'DEV' in os.environ
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-adamalive-sculpturedrfa-aw4zyb1un53.ws.codeinstitute-ide.net',                
                 '.herokuapp.com', 'sculpture-drf-api-2aa8ed66624d.herokuapp.com'
