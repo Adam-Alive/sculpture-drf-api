aedca7d387f0d84676ba8229b15fa0408bcf8bb9
Adam Alive <adamalive.email@gmail.com>
2024-10-21 10:09:31 +0000

    Add fields to BookingSerializer

--- bookings/serializers.py
+++ bookings/serializers.py
@@ -4,7 +4,12 @@ from .models import Booking
 
 class BookingSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
-    # talk = serializers.ReadOnlyField(source='talk.title')
+    is_owner = serializers.SerializerMethodField()
+    profile_id = serializers.ReadOnlyField(source='owner.profile.id')
+
+    def get_is_owner(self, obj):
+        request = self.context['request']
+        return request.user == obj.owner
 
     class Meta:
         model = Booking
