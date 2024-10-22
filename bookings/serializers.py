5521f65b5b5cbaba184c2f5715225e7212b0eee5
Adam Alive <adamalive.email@gmail.com>
2024-10-22 18:41:41 +0000

    Update BookingSerializer

--- bookings/serializers.py
+++ bookings/serializers.py
@@ -6,11 +6,6 @@ class BookingSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
 
-    
-    # talk_id = serializers.SerializerMethodField()
-    # profile_id = serializers.ReadOnlyField(source='owner.profile.id')
-    # talk_id = serializers.ReadOnlyField(source='owner.talk.id')  ????
-
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
