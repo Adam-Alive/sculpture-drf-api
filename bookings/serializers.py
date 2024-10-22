b1bf6c44178f799c3fb1ad61f13ee6548770fb4e
Adam Alive <adamalive.email@gmail.com>
2024-10-22 17:00:05 +0000

    Add talk_id to Booking.Serializer for testing

--- bookings/serializers.py
+++ bookings/serializers.py
@@ -6,6 +6,9 @@ class BookingSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
 
+    # profile_id = serializers.ReadOnlyField(source='owner.profile.id')
+    # talk_id = serializers.ReadOnlyField(source='owner.profile.id')  ????
+
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
