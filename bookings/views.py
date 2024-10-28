033f19410be93dd8f72cb41f70a41aba9e2e1460
Adam Alive <adamalive.email@gmail.com>
2024-10-28 15:23:26 +0000

    Add filterset fields to BookingList view

--- bookings/views.py
+++ bookings/views.py
@@ -1,4 +1,5 @@
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions, filters
+from django_filters.rest_framework import DjangoFilterBackend
 from drf_api.permissions import IsOwnerOrReadOnly
 from .models import Booking
 from .serializers import BookingSerializer
@@ -11,6 +12,12 @@ class BookingList(generics.ListCreateAPIView):
     serializer_class = BookingSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     queryset = Booking.objects.all()
+    filter_backends = [
+         DjangoFilterBackend,
+    ]
+    filterset_fields = [
+        'owner__profile',
+    ]
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
