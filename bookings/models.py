2f68b973c3c8fc39e9ccd6f361dcab9a70c33882
Adam Alive <adamalive.email@gmail.com>
2024-10-30 21:20:07 +0000

    Add unique_together constraint to Booking model

--- bookings/models.py
+++ bookings/models.py
@@ -22,7 +22,8 @@ class Booking(models.Model):
         """
         List bookings by date in ascending order.
         """
-        ordering = ['date']        
+        ordering = ['date']
+        unique_together = ['owner', 'title']
 
     def __str__(self):
         return f"{self.owner}'s booking for {self.title}"
