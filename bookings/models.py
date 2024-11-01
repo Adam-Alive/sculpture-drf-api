79db7aada91b329ee9cdb670b65ba18d46bda0a3
Adam Alive <adamalive.email@gmail.com>
2024-11-01 17:34:57 +0000

    Amend Booking model and BookingSerializer

--- bookings/models.py
+++ bookings/models.py
@@ -1,17 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
-from talks.models import Talk
-import logging
-logger = logging.getLogger('django.server')
 
 
 class Booking(models.Model):
     """
     Booking model for user to register for talk events.
     """
-    owner = models.ForeignKey(User, on_delete=models.CASCADE, null=False, blank=False)
-    talk = models.ForeignKey(Talk, on_delete=models.CASCADE, default=14)
-    title = models.CharField(max_length=200, null=False, blank=False)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
     speaker = models.CharField(max_length=100)
     date = models.DateField(null=True, blank=True)
     start_time = models.TimeField(null=True, blank=True)
@@ -22,13 +18,12 @@ class Booking(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-    logger.error('some important infos')
     class Meta:
         """
         List bookings by date in ascending order.
         """
         ordering = ['date']
-        unique_together = (('owner', 'talk'),)
+        # unique_together = ['owner', 'title']
 
     def __str__(self):
         return f"{self.owner}'s booking for {self.title}"
